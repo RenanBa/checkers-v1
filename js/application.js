6469876ec0f942faddf8447a343b775d845ee418
RenanBa <renanbarbosadesouza@gmail.com>
2017-10-25 12:18:42 -0700

    Change squares id to be the same as the array id and change the css squares id

--- js/application.js
+++ js/application.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   console.log("Ready!");
   // initialize the game and display a board
   var game = new Game();
-  PopulateBoard(game.flattenBoard());
+  PopulateBoard(game.flattenBoard);
 
   //Handles clicks on the checkers board
   $(".board").on("click", "a", function(e){
@@ -23,11 +23,11 @@ var PopulateBoard = function(board){
   // this forEach run over the flatten board and create elements for each piece and for the available spots which are all set the position with css
   board.forEach(function(value, index){
     if (value == "green"){
-      $(".board").append($.parseHTML('<a href="'+(index+1)+'" id="square'+(index+1)+'" class="green"></a>'));
+      $(".board").append($.parseHTML('<a href="'+(index)+'" id="square'+(index)+'" class="green"></a>'));
     } else if (value == "blue"){
-      $(".board").append($.parseHTML('<a href="'+(index+1)+'" id="square'+(index+1)+'" class="blue"></a>'));
+      $(".board").append($.parseHTML('<a href="'+(index)+'" id="square'+(index)+'" class="blue"></a>'));
     } else if (value == "empty"){
-      $(".board").append($.parseHTML('<a href="'+(index+1)+'" id="square'+(index+1)+'" class="empty"></a>'));
+      $(".board").append($.parseHTML('<a href="'+(index)+'" id="square'+(index)+'" class="empty"></a>'));
     }
   })
 }
